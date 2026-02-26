62b4bd7e85001ab65936b04ea70d6f70de039ba4
Muri,sat,cos <costasantanamurillo@gmail.com>
2026-02-25 21:41:26 -0300

    Update App component layout and styles

--- App.tsx
+++ App.tsx
@@ -1,42 +1,43 @@
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  return(
+  return (
+
     <View style={styles.container}>
-    <Text style={styles.text}> FOI </Text>
+      <Text style={styles.text}> crie uma conta </Text>{' '}
+      <TextInput style={styles.input} placeholder="EMAIL" />
+      {'.'}
+      <TextInput style={styles.input} placeholder="SENHA" />
+      {'.'}
+      <Button title="ENTRA" color="#0acf06"/>
+      {'.'}
+      <Button title="ENTRA" color="#db2525"/>
 
-    <TextInput style={styles.input}
-    placeholder="Meu primeiro App"
-     />
-
-     <Button title="SALVAR" />
 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
- text:{
-   fontSize: 30,
-   color: '#27F5F2'
- },
+  text: {
+    fontSize: 45,
+    color: '#27F5F2',
+    padding: 10,
+  },
 
-container:{
-  flex: 1,
-  justifyContent: "center",
-  alignItems: "center",
-  backgroundColor: "#000000"
-},
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000',
+  },
 
-input:{
-  backgroundColor: "#C7C7C7",
-  textAlign: "center",
-  width: "50%",
-  borderRadius: 4,
-  height: "5%",
-  color: "#737272"
-},
-
-
-
+  input: {
+    backgroundColor: '#C7C7C7',
+    textAlign: 'center',
+    width: '60%',
+    borderRadius: 3,
+    height: '7%',
+    color: '#737272',
+  },
 });
